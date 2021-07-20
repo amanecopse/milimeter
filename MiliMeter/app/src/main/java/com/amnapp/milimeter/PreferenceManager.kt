24d4491a61639f00608d1b7103c267dcdfe2be3b
amanecopse <amanecopse@users.noreply.github.com>
2021-07-20 13:31:25 +0900

    UI통합안 수정
    -예시 레이아웃 추가
    -테마 변경 버튼과 작동 소스 추가

--- MiliMeter/app/src/main/java/com/amnapp/milimeter/PreferenceManager.kt
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/PreferenceManager.kt
@@ -9,10 +9,20 @@ class PreferenceManager {
     private val LOGIN_DATA_ID = "id"
     private val LOGIN_DATA_PW = "pw"
     private val LOGIN_DATA_GC = "gc"
+    private val SETTINGS_DATA_THEME = "theme"
+
+    companion object{
+        val THEME_DARK = "dark theme"
+        val THEME_DEFAULT = "default theme"
+    }
 
 
     private fun getLoginPreference(context: Context): SharedPreferences? {
         return context.getSharedPreferences(PREFERENCE_NAME_LOGIN, Context.MODE_PRIVATE)
+    }
+
+    private fun getSettingsPreference(context: Context): SharedPreferences? {
+        return context.getSharedPreferences(PREFERENCE_NAME_SETTINGS, Context.MODE_PRIVATE)
     }
 
     fun setLoginData(context: Context, id:String, pw:String, groupCode:String){
@@ -24,12 +34,25 @@ class PreferenceManager {
         editor?.apply()
     }
 
+    fun setThemeData(context: Context, theme: String){
+        val prefs = getSettingsPreference(context)
+        val editor = prefs?.edit()
+        editor?.putString(SETTINGS_DATA_THEME,theme)
+        editor?.apply()
+    }
+
     fun getLoginData(context: Context): Array<String?> {
         val prefs = getLoginPreference(context)
         val id = prefs?.getString(LOGIN_DATA_ID, "")
         val pw = prefs?.getString(LOGIN_DATA_PW, "")
         val groupCode = prefs?.getString(LOGIN_DATA_GC, "")
         return arrayOf(id, pw, groupCode)
+    }
+
+    fun getThemeData(context: Context): String? {
+        val prefs = getSettingsPreference(context)
+        val theme = prefs?.getString(SETTINGS_DATA_THEME, "")
+        return theme
     }
 
 }
--- MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/ThemeExampleActivity.kt
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/ThemeExampleActivity.kt
@@ -1,13 +1,45 @@
 package com.amnapp.milimeter.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.amnapp.milimeter.PreferenceManager
 import com.amnapp.milimeter.R
 
 class ThemeExampleActivity : AppCompatActivity() {
+    var mTheme: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setTheme(R.style.DarkAppTheme)
+        loadTheme()
         setContentView(R.layout.activity_theme_example)
+
+        findViewById<Button>(R.id.changeThemeBt).setOnClickListener {
+            val pm = PreferenceManager()
+            if(mTheme == PreferenceManager.THEME_DARK){
+                pm.setThemeData(this, PreferenceManager.THEME_DEFAULT)
+            }
+            else{
+                pm.setThemeData(this, PreferenceManager.THEME_DARK)
+            }
+            Toast.makeText(this, pm.getThemeData(this), Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, ThemeExampleActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun loadTheme() {
+        val pm = PreferenceManager()
+        mTheme = pm.getThemeData(this)
+        if(mTheme == PreferenceManager.THEME_DARK){
+            setTheme(R.style.DarkAppTheme)
+        }
+        else{
+            setTheme(R.style.AppTheme)
+        }
     }
 }
