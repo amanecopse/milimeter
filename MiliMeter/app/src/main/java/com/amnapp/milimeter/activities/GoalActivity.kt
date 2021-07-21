8b177598e005312da5b4de77164b28a14c8d2108
dhl0929 <dhdh0929@naver.com>
2021-07-21 23:23:25 +0900

    이다현 파일

--- MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/GoalActivity.kt
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/GoalActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
+import com.amnapp.milimeter.R
 
 class GoalActivity : AppCompatActivity() {
     override fun setContentView(layoutResID: Int) {
--- MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/HomeActivity.kt
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/HomeActivity.kt
@@ -1,30 +1,12 @@
-package com.amnapp.milimeter.activities
+package com.example.myapplication
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.amnapp.milimeter.databinding.ActivityHomeBinding
+import android.os.Bundle
+import com.amnapp.milimeter.R
 
 class HomeActivity : AppCompatActivity() {
-
-    lateinit var binding: ActivityHomeBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.loginBt.setOnClickListener{
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-        binding.recordBt.setOnClickListener{
-            val intent = Intent(this, TrainingRecordActivity::class.java)
-            startActivity(intent)
-        }
-        binding.themeExampleBt.setOnClickListener{
-            val intent = Intent(this, ThemeExampleActivity::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_home)
     }
 }
