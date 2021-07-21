f11d3280c08acc8b646dbc2b6e690d6eccdc785d
dhl0929 <84281744+dhl0929@users.noreply.github.com>
2021-07-21 19:20:03 +0900

    Add files via upload

---
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/GoalActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+
+class GoalActivity : AppCompatActivity() {
+    override fun setContentView(layoutResID: Int) {
+        super.setContentView(R.layout.activity_goal)
+    }
+
+
+}
