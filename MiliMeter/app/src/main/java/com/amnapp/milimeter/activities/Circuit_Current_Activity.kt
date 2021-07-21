770b470005b437e9bcffb4e0057e4625bc2c9625
kodh0206 <69523224+kodh0206@users.noreply.github.com>
2021-07-22 06:15:39 +0900

    전장순환 운동 현재상태(그래프 등 함수 미구현)

---
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/Circuit_Current_Activity.kt
@@ -0,0 +1,33 @@
+package com.amnapp.milimeter
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class Circuit_Current_Activity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_circuit_current)
+
+        //버튼
+        var CurrentButton = findViewById<Button>(R.id.all)
+        CurrentButton.setOnClickListener {
+            val currentintent = Intent(this, Current_Activity::class.java)
+            startActivity(currentintent)
+        }
+        //레그턱
+        var legButton = findViewById<Button>(R.id.legtuckbutton)
+        legButton.setOnClickListener {
+            val legintent = Intent(this, Legtuck_Current_Activity::class.java)
+            startActivity(legintent)
+        }
+        //240m가는 버튼
+        var runButton = findViewById<Button>(R.id.run)
+        runButton.setOnClickListener {
+            val runintent = Intent(this, Running_Current_Activity::class.java)
+            startActivity(runintent)
+
+        }
+    }
+}
