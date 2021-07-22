2ebe27e6e7d57a9471c0a38b56db376c9123e296
amanecopse <amanecopse@users.noreply.github.com>
2021-07-22 18:30:00 +0900

    홈화면에 디버그창으로 가는 버튼 추가
    홈화면이 기능을 하고 있지 않는 상황이라 디버그창과 그곳으로 가는 버튼을 추가함

---
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/DebugActivity.kt
@@ -0,0 +1,32 @@
+package com.amnapp.milimeter.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amnapp.milimeter.R
+import com.amnapp.milimeter.databinding.ActivityDebugBinding
+import com.amnapp.milimeter.databinding.ActivitySignInBinding
+
+class DebugActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDebugBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDebugBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginActivityBt.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.trainingRecordActivityBt.setOnClickListener {
+            val intent = Intent(this, TrainingRecordActivity::class.java)
+            startActivity(intent)
+        }
+        binding.themeExampleActivity.setOnClickListener {
+            val intent = Intent(this, ThemeExampleActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/HomeActivity.kt
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/HomeActivity.kt
@@ -1,12 +1,20 @@
 package com.amnapp.milimeter.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.amnapp.milimeter.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        findViewById<Button>(R.id.debugBt).setOnClickListener {//디버그 창으로 가는 코드
+            val intent = Intent(this, DebugActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
