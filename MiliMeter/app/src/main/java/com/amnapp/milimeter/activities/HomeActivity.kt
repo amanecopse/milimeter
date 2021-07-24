b85fdf90629b7c5f876f6211b0d07c6dfda6d5f7
amanecopse <amanecopse@users.noreply.github.com>
2021-07-24 22:33:44 +0900

    자동 로그인 구현

--- MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/HomeActivity.kt
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/HomeActivity.kt
@@ -2,11 +2,18 @@ package com.amnapp.milimeter.activities
 
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.DatePicker
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import com.amnapp.milimeter.AccountManager
+import com.amnapp.milimeter.PreferenceManager
 import com.amnapp.milimeter.R
+import com.amnapp.milimeter.UserData
 import com.amnapp.milimeter.databinding.ActivityHomeBinding
 import com.amnapp.milimeter.databinding.ActivitySettingBinding
 import java.util.*
@@ -14,17 +21,27 @@ import java.util.*
 class HomeActivity : AppCompatActivity() {
 
     val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
+    lateinit var mLoadingDialog: AlertDialog//로딩화면임. setProgressDialog()를 실행후 mLoadingDialog.show()로 시작
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        initUI()
+        autoLogin()
+    }
 
-        findViewById<Button>(R.id.debugBt).setOnClickListener {//디버그 창으로 가는 코드
+    private fun initUI() {
+        setProgressDialog()
+
+        binding.debugBt.setOnClickListener {//디버그 창으로 가는 코드
             val intent = Intent(this, DebugActivity::class.java)
             startActivity(intent)
         }
-
+        binding.resultBt.setOnClickListener {
+            val bodyintent =Intent(this,ResultActivity::class.java)
+            startActivity(bodyintent)
+        }
         //Dday 날짜설정
 //        binding.DdayBt.setOnClickListener {
 //            val today = GregorianCalendar()
@@ -39,12 +56,65 @@ class HomeActivity : AppCompatActivity() {
 //         }, year, month, date)
 //        dlg.show()
 //      }
+    }
 
+    private fun autoLogin() {
+        mLoadingDialog.show() // 로딩화면 실행
 
-        findViewById<Button>(R.id.resultBt).setOnClickListener {
-            val bodyintent =Intent(this,ResultActivity::class.java)
-            startActivity(bodyintent)
+        val pm = PreferenceManager()
+        val loginDataArray = pm.getLoginData(this)// 로그인 데이터를 로컬저장소에서 로드함
+
+        if(loginDataArray[0].isNullOrEmpty()// 로그인 정보 없는 경우
+            ||loginDataArray[1].isNullOrEmpty()
+            ||loginDataArray[2].isNullOrEmpty()){
+            mLoadingDialog.dismiss()//로딩해제
+            return// 로딩해제하고 빠져나옴
         }
 
+        AccountManager().login(loginDataArray[0]!!, loginDataArray[1]!!, loginDataArray[2]!!, object: AccountManager.UICallBack{
+            override fun whatToDo() {
+                return
+            }
+            override fun whatToDoWithMessage(result: String) {
+                if(result == AccountManager.LOGIN_SUCCESS){
+                    Toast.makeText(this@HomeActivity, "로그인", Toast.LENGTH_SHORT).show()
+                }
+                mLoadingDialog.dismiss() // 로딩해제
+            }
+        })
+    }
+
+    fun setProgressDialog() {
+        val llPadding = 30
+        val ll = LinearLayout(this)
+        ll.orientation = LinearLayout.HORIZONTAL
+        ll.setPadding(llPadding, llPadding, llPadding, llPadding)
+        ll.gravity = Gravity.CENTER
+        var llParam = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        llParam.gravity = Gravity.CENTER
+        ll.layoutParams = llParam
+        val progressBar = ProgressBar(this)
+        progressBar.isIndeterminate = true
+        progressBar.setPadding(0, 0, llPadding, 0)
+        progressBar.layoutParams = llParam
+        llParam = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        llParam.gravity = Gravity.CENTER
+        val tvText = TextView(this)
+        tvText.text = "Loading ..."
+        tvText.setTextColor(Color.parseColor("#000000"))
+        tvText.textSize = 20f
+        tvText.layoutParams = llParam
+        ll.addView(progressBar)
+        ll.addView(tvText)
+        val builder = AlertDialog.Builder(this)
+        builder.setCancelable(false)
+        builder.setView(ll)
+        mLoadingDialog = builder.create()
     }
 }
