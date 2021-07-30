29abe0c662a762213254131e23a079e13cef92c0
amanecopse <amanecopse@users.noreply.github.com>
2021-07-30 14:20:08 +0900

    테마예시에 하단메뉴추가
    메뉴변경 체크박스로 표시전환할 수 있음

--- MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/ThemeExampleActivity.kt
+++ MiliMeter/app/src/main/java/com/amnapp/milimeter/activities/ThemeExampleActivity.kt
@@ -3,19 +3,26 @@ package com.amnapp.milimeter.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.amnapp.milimeter.PreferenceManager
 import com.amnapp.milimeter.R
+import com.amnapp.milimeter.databinding.ActivityLoginBinding
+import com.amnapp.milimeter.databinding.ActivityThemeExampleBinding
 
 class ThemeExampleActivity : AppCompatActivity() {
     var mTheme: String? = null
+    lateinit var binding: ActivityThemeExampleBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loadTheme()
-        setContentView(R.layout.activity_theme_example)
+        binding = ActivityThemeExampleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.changeThemeBt).setOnClickListener {
+        binding.changeThemeBt.setOnClickListener {
             val pm = PreferenceManager()
             if(mTheme == PreferenceManager.THEME_DARK){
                 pm.setThemeData(this, PreferenceManager.THEME_DEFAULT)
@@ -28,6 +35,19 @@ class ThemeExampleActivity : AppCompatActivity() {
             val intent = Intent(this, ThemeExampleActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        binding.changeBottomMenuCb.setOnCheckedChangeListener { buttonView, isChecked ->
+            when(isChecked){
+                true->{
+                    binding.menuBottomLl.visibility = View.VISIBLE
+                    binding.bottomLl.visibility = View.GONE
+                }
+                false->{
+                    binding.menuBottomLl.visibility = View.GONE
+                    binding.bottomLl.visibility = View.VISIBLE
+                }
+            }
         }
 
     }
